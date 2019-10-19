b4ba825174af3b68c87a7dfa7280af61af31c8f0
zhangrui3 <zhangrui3@lvwan.com>
2019-10-19 10:03:06 +0800

    hadoop数据压缩格式不支持分片修改

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,4 +5,12 @@
  * @Date 2019/08/30
  */
 public class Solution {
+    public static void main(String[] args) {
+        String str = null;
+        String[] split = str.split(",");
+        for (String s : split) {
+            System.out.println(s);
+        }
+
+    }
 }
