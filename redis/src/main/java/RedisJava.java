970ca5e1d795fd2bbba309a7f31194e74dcc9802
zhangrui3 <zhangrui3@lvwan.com>
2019-11-15 09:40:08 +0800

    redis java interface call

---
+++ redis/src/main/java/RedisJava.java
@@ -0,0 +1,46 @@
+import redis.clients.jedis.Jedis;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @ClassName
+ * @Description
+ * @Author
+ * @Date 2019/11/14
+ */
+public class RedisJava {
+    public static void main(String[] args) {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+
+
+        jedis.set("key7","zhangrui7");
+        System.out.println("the value of the key7 is: "+ jedis.get("key7"));
+
+
+        //存储数据到列表中
+        jedis.lpush("site-list", "Runoob");
+        jedis.lpush("site-list", "Google");
+        jedis.lpush("site-list", "Taobao");
+        // 获取存储的数据并输出
+        List<String> list = jedis.lrange("site-list", 0 ,2);
+        for(int i=0; i<list.size(); i++) {
+            System.out.println("列表项为: "+list.get(i));
+        }
+
+
+        // 获取数据并输出
+        Set<String> keys = jedis.keys("*");
+        Iterator<String> it=keys.iterator() ;
+        while(it.hasNext()){
+            String key = it.next();
+            System.out.println(key);
+        }
+
+    }
+}
